d21b5f70a32e70c6e576192254409ba0fa616970
Jimmy P <mail@thejimmyp.com>
2013-01-26 11:42:36 +0800

    Finished ex10

--- HelloWorld/HelloWorldServer/EndpointConfig.cs
+++ HelloWorld/HelloWorldServer/EndpointConfig.cs
@@ -7,9 +7,15 @@ namespace HelloWorldServer
     {
         public void Init()
         {
-            NServiceBus.Configure.With().DefaultBuilder()
-                .XmlSerializer("http://acme.com");
+            NServiceBus.Configure.With()
+                .DefaultBuilder()
+                .XmlSerializer("http://acme.com")
+                .RunCustomAction(() =>
+                Configure.Instance.Configurer.ConfigureComponent<SaySomething>(
+                                     DependencyLifecycle.SingleInstance)
+                );
         }
+
 
         public void SpecifyOrder(Order order)
         {
