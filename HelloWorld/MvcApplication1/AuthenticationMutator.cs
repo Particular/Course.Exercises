98104ebdbb9cbee690d0508b4fda765d102cd316
Jimmy P <mail@thejimmyp.com>
2013-01-26 14:56:15 +0800

    Finished ex13

---
+++ HelloWorld/MvcApplication1/AuthenticationMutator.cs
@@ -0,0 +1,24 @@
+﻿using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.MessageMutator;
+using NServiceBus.Unicast.Transport;
+
+namespace MvcApplication1
+{
+    public class AuthenticationMutator : IMutateOutgoingTransportMessages,
+                                         INeedInitialization
+    {
+        public void MutateOutgoing(object[] messages, TransportMessage transportMessage)
+        {
+            //in a real life scenario you would probably get the user from
+            // HttpContext.Current.Request.Params[“user”] or similar 
+            transportMessage.Headers["user"] = "udi";
+        }
+
+        public void Init()
+        {
+            Configure.Instance.Configurer
+                .ConfigureComponent<AuthenticationMutator>(DependencyLifecycle.InstancePerCall);
+        }
+    }
+}
