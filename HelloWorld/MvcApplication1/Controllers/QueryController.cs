4d1e2421482e8a6c51669c942f5c3c8af611b2fb
Jimmy P <mail@thejimmyp.com>
2013-01-26 15:33:18 +0800

    Finished ex16

---
+++ HelloWorld/MvcApplication1/Controllers/QueryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Messages;
+
+namespace MvcApplication1.Controllers
+{
+    public class QueryController : Controller
+    {
+        public void Index()
+        {
+            MvcApplication.Bus.Send(new Query() {NumberOfResponses = 10});
+        }    
+    }
+}
