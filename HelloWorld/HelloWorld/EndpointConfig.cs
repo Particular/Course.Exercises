c28fb1ce7ede389f026aaf099967f658fc421fa5
danielmarbach <daniel.marbach@openplace.net>
2013-03-13 16:12:52 +0100

    Upgrade to NSB 4.0 unstable 2852

--- HelloWorld/HelloWorld/EndpointConfig.cs
+++ HelloWorld/HelloWorld/EndpointConfig.cs
@@ -1,16 +1,15 @@
-using log4net;
-
 namespace HelloWorld 
 {
     using NServiceBus;
+    using NServiceBus.Logging;
 
 	/*
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://nservicebus.com/GenericHost.aspx
 	*/
-    public class EndpointConfig : IConfigureThisEndpoint, IWantToRunAtStartup
+    public class EndpointConfig : IConfigureThisEndpoint, IWantToRunWhenBusStartsAndStops
     {
-        public void Run()
+        public void Start()
         {
             LogManager.GetLogger("EndpointConfig").Info("Hello World!");
         }
