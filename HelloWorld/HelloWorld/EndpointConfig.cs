5846372b7438df7927639124e47511f583b178e5
sfarmar <s.farmar@gmail.com>
2014-02-07 16:20:28 +0000

    updated to 4.3.3

--- HelloWorld/HelloWorld/EndpointConfig.cs
+++ HelloWorld/HelloWorld/EndpointConfig.cs
@@ -1,16 +1,15 @@
-using log4net;
-
 namespace HelloWorld 
 {
     using NServiceBus;
+    using NServiceBus.Logging;
 
 	/*
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
-		can be found here: http://nservicebus.com/GenericHost.aspx
+		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-    public class EndpointConfig : IConfigureThisEndpoint, IWantToRunAtStartup
+	public class EndpointConfig : IConfigureThisEndpoint, IWantToRunWhenBusStartsAndStops, AsA_Client
     {
-        public void Run()
+        public void Start()
         {
             LogManager.GetLogger("EndpointConfig").Info("Hello World!");
         }
