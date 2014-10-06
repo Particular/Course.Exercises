e8a28d1c4c3ea2c60c7ac6d05172e8be0858f8cc
Daniel Marbach <daniel.marbach@openplace.net>
2014-10-06 10:10:27 +0200

    Upgrade to NSB v5

--- HelloWorld/HelloWorld/EndpointConfig.cs
+++ HelloWorld/HelloWorld/EndpointConfig.cs
@@ -1,22 +1,27 @@
-namespace HelloWorld 
+namespace HelloWorld
 {
     using NServiceBus;
     using NServiceBus.Logging;
+    using NServiceBus.Persistence;
 
-	/*
+    /*
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-	public class EndpointConfig : IConfigureThisEndpoint, IWantToRunWhenBusStartsAndStops, AsA_Client
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Client, IWantToRunWhenBusStartsAndStops
     {
+        public void Customize(BusConfiguration configuration)
+        {
+           configuration.UsePersistence<RavenDBPersistence>();
+        }
+
         public void Start()
         {
-            LogManager.GetLogger("EndpointConfig").Info("Hello World!");
+            LogManager.GetLogger("EndpointConfig").Info("Hello Distributed World!");
         }
 
         public void Stop()
         {
-
         }
     }
 }
