0966080e5ed7a38824ce5f2ca8f926eac97015ba
Daniel Marbach <daniel.marbach@openplace.net>
2014-10-06 10:50:25 +0200

    Fixing IWantToRunWHenBusStartsAndStops

--- HelloWorld/HelloWorld/EndpointConfig.cs
+++ HelloWorld/HelloWorld/EndpointConfig.cs
@@ -1,3 +1,5 @@
+using NServiceBus.Logging;
+
 namespace HelloWorld
 {
     using NServiceBus;
@@ -7,11 +9,20 @@ namespace HelloWorld
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Client
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Client, IWantToRunWhenBusStartsAndStops
     {
         public void Customize(BusConfiguration configuration)
         {
            configuration.UsePersistence<RavenDBPersistence>();
+        }
+
+        public void Start()
+        {
+            LogManager.GetLogger("EndpointConfig").Info("Hello Distributed World!");
+        }
+
+        public void Stop()
+        {
         }
     }
 }
