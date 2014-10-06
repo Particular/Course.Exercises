fdf1ee5089129f5df1f70666f1485ce5332fdc1f
Daniel Marbach <daniel.marbach@openplace.net>
2014-10-06 13:04:19 +0200

    Upgrade to NSB v5

--- HelloWorld/HelloWorld/EndpointConfig.cs
+++ HelloWorld/HelloWorld/EndpointConfig.cs
@@ -1,17 +1,22 @@
-namespace HelloWorld 
+using Messages;
+
+namespace HelloWorld
 {
-    using Messages;
-    using NServiceBus;	
-	/*
+    using NServiceBus;
+    using NServiceBus.Persistence;
+
+    /*
 		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
 		can be found here: http://particular.net/articles/the-nservicebus-host
 	*/
-    public class EndpointConfig : IConfigureThisEndpoint, AsA_Client, IWantCustomInitialization
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Client
     {
-        public void Init()
+        public void Customize(BusConfiguration configuration)
         {
-            Configure.With()
+            configuration.Conventions()
                 .DefiningMessagesAs(t => t.Assembly == typeof(RequestMessage).Assembly && t.Name.EndsWith("Message"));
+
+            configuration.UsePersistence<RavenDBPersistence>();
         }
     }
 }
