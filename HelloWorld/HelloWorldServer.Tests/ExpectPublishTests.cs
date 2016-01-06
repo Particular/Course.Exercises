e0ed07def6fed283d449d747c2f9dbe6dee59a0e
sfarmar <s.farmar@gmail.com>
2016-01-06 11:07:04 +0000

    Added Expect Publish Tests

---
+++ HelloWorld/HelloWorldServer.Tests/ExpectPublishTests.cs
@@ -0,0 +1,51 @@
+﻿namespace HelloWorldServer.Tests
+{
+    using NServiceBus;
+    using NServiceBus.Testing;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExpectPublishTests
+    {
+
+        [Test]
+        public void Run()
+        {
+            Test.Initialize(c => c.Conventions().DefiningEventsAs(t => t == typeof (ICatalogue)));
+            
+            var cmd = new Catalogue()
+            {
+                CatalogueCode = "TEST"
+            };
+            Test.Handler<CatalogueAddedCommandHandler>()
+                .ExpectPublish<ICatalogue>(e => e.CatalogueCode == cmd.CatalogueCode)
+                .OnMessage(cmd);
+
+        }
+    }
+
+    public interface ICatalogue
+    {
+        string CatalogueCode { get; set; }
+    }
+
+    public class Catalogue
+    {
+        public string CatalogueCode { get; set; }
+    }
+
+    public class CatalogueEvent : ICatalogue
+    {
+        public string CatalogueCode { get; set; }
+    }
+
+    public class CatalogueAddedCommandHandler : IHandleMessages<Catalogue>
+    {
+        public IBus Bus { get; set; }
+
+        public void Handle(Catalogue message)
+        {
+            Bus.Publish<ICatalogue>(e => e.CatalogueCode = message.CatalogueCode);
+        }
+    }
+}
