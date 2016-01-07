13c9e342dbd759268add30dc9a25b289c3509d0e
sfarmar <s.farmar@gmail.com>
2016-01-07 12:05:15 +0000

    adding a timeout test

--- Saga/HelloWorld.Sagas.Tests/SagaTests.cs
+++ Saga/HelloWorld.Sagas.Tests/SagaTests.cs
@@ -3,6 +3,7 @@
     using System;
     using Messages;
     using Messages.Commands;
+    using Messages.Messages;
     using NServiceBus.Testing;
     using NUnit.Framework;
     using Saga;
@@ -21,6 +22,24 @@
             Test.Saga<ShippingSaga>(orderId)
                 .WhenReceivesMessageFrom("HelloWorld.MessageSender")
                 .ExpectSend<ShipToFedEx>(m => m.OrderId = orderId)
+                .When(saga => saga.Handle(message));
+        }
+
+        [Test]
+        public void TimeoutTest()
+        {
+            //.ExpectTimeoutToBeSetAt<ReadyToSellTimeout>((state, span) => span == dtPurchase)
+            //.ExpectPublish<LoanStatusUpdated>(r => r.Status == LoanStatusEnum.Funded)
+            //.When(s => s.Handle(new Funded { FundedDate = DateTime.Now, LoanGUID = guidTest }))
+
+            var orderId = Guid.NewGuid();
+            var message = new ShippOrder { OrderId = orderId };
+
+            Test.Initialize();
+
+            Test.Saga<ShippingSaga>(orderId)
+                //.ExpectTimeoutToBeSetAt<FedexTimedOut>((m, at) => at == DateTime.UtcNow.AddSeconds(15))
+                .ExpectTimeoutToBeSetAt<FedexTimedOut>((m, at) => at == TimeSpan.FromSeconds(15))
                 .When(saga => saga.Handle(message));
         }
     }
